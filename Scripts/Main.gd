db72d624d16b19098fbde392a81934ff13c4548d
Bea <beagraboloza@gmail.com>
2021-12-07 21:45:58 -0300

    Smol documentacion

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,33 +1,55 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Member variables
 var stars
 var Star
+var Poligono = []
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	stars = []
 	Star = preload("../Escenas/Star.tscn")
-	#var new_node = MyNode.instance()
-	#if you want to change something in the node (for example the position here):
-	#new_node.position = new_position
-	#scene.add_child(new_node)
-	# Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+# warning-ignore:unused_argument
+# Interact with input
 func _input(event):
 	if Input.is_action_pressed("ui_click"):
-		var new_star = Star.instance()
-		new_star.place = get_global_mouse_position()
-		new_star.position = new_star.place
+		var new_star = createStar()
 		stars.append(new_star)
 		get_tree().get_current_scene().add_child(new_star)
-		
+		update()
+	if Input.is_action_pressed("ui_der_click"):
+		var new_star = createStar()
+		for star in stars:
+			if star == new_star:
+				stars.remove(star)
+				get_tree().get_current_scene().remove_child(star)
+		update()
+
+# Creates a new instance of Star in the mouse position
+func createStar():
+	var new_star = Star.instance()
+	new_star.set_place(get_global_mouse_position())
+	return new_star		
+
+# return the positions of the stars
+func get_positions():
+	var positions = []
+	for star in stars:
+		positions.append(star.get_place())
+	return positions
+
+# Se llama cada vez que se hace update()
+# No se porque no dibuja al poligono :C
+func _draw():
+	var colors = PoolColorArray([Color(1,1,1)])
+	if stars.size() >=3:
+		Poligono = get_positions()
+		draw_polygon(Poligono,colors)
--- Scripts/Star.gd
+++ Scripts/Star.gd
@@ -1,9 +1,7 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Member variables
 var place 
 
 
@@ -11,7 +9,22 @@ var place
 func _ready():
 	place = Vector2(0,0)
 
-
+# Return the place of the star 
+func get_place() -> Vector2:
+	return place
+	
+# Sets the place in the star and in its position
+func set_place(new_place: Vector2):
+	place = new_place
+	position = place
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+# To see if a star is in the same position of other star
+func equals(star):
+	if star.get_class() == "Star":
+		if star.get_place() == place:
+			return true
+	return false
