e1bf40aa547a04499a8c537df804b7ce4f0ffac6
Beltrán Amenábar <beltra.am@gmail.com>
2021-12-28 18:08:49 -0300

    fix(cgal_test): fix the cmakefile to actually work with cgal. fix the using defs in the example

--- src/cgalexample.cpp
+++ src/cgalexample.cpp
@@ -5,13 +5,12 @@
 #include <numeric>
 #include <vector>
 
-using CGAL::Exact_predicates_inexact_constructions_kernel = K;
-using K::Point_2 = Point_2;
-using CGAL::Convex_hull_traits_adapter_2<K, CGAL::Pointer_property_map<Point_2>::type> =
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef K::Point_2 Point_2;
+typedef CGAL::Convex_hull_traits_adapter_2<K, CGAL::Pointer_property_map<Point_2>::type>
     Convex_hull_traits_2;
 
 int main() {
-
   std::vector<Point_2> points = {Point_2(10, 0), Point_2(10, 0), Point_2(0, 10),  Point_2(1, 1),
                                  Point_2(3, 4),  Point_2(0, 0),  Point_2(10, 10), Point_2(2, 6)};
   std::vector<std::size_t> indices(points.size()), out;
