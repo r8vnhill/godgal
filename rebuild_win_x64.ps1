203ff7881eac5b2a1efdf75cbee629f457d811c7
R8V <ignacio.slater@ug.uchile.cl>
2021-12-28 10:53:08 -0300

    refactor(build): Improved design and documentation of build script

--- rebuild_win_x64.ps1
+++ rebuild_win_x64.ps1
@@ -1,20 +1,58 @@
-try {
-  $originalLocation = Get-Location
-  $originalErrorAction = $ErrorActionPreference
-
-  $ErrorActionPreference = 'Stop'
-
-  if (Test-Path -Path build) {
-    Remove-Item build -Recurse -Force -Verbose
+function Script:Invoke-BuildCMake {
+  try {
+    New-Item -Path build -ItemType Directory
+    Set-Location build
+    cmake.exe -S .. -DCMAKE_BUILD_TYPE=Debug
+    cmake.exe --build . --target libgdexample
+  } catch {
+    Write-Error 'There was an error while building the project'
+    throw
   }
-  New-Item -Path build -ItemType Directory
-  Set-Location build
+  <#
+  .SYNOPSIS
+    Builds the CMake project in the current scope.
+  #>
+}
 
-  cmake.exe -S .. -DCMAKE_BUILD_TYPE=Debug
-  cmake.exe --build . --target libgdexample
+function Script:Invoke-CleanCMake {
+  try {
+    if (Test-Path -Path build) {
+      Remove-Item build -Recurse -Force -Verbose
+    }
+  } catch {
+    Write-Error 'There was an error while cleaning the project'
+    throw
+  }
+  <#
+  .SYNOPSIS
+    Remove files from previous builds.
+  #>
+}
 
-  Set-Location $originalLocation
-  Copy-Item -Path .\build\Debug\ -Destination .\project\bin\win64 -Recurse -Force -Verbose
+function Script:Sync-Binaries {
+  try {
+    Set-Location $originalLocation
+    Copy-Item -Path .\build\Debug\ -Destination .\project\bin\win64 -Recurse -Force -Verbose
+  } catch {
+    Write-Error 'There was an error while syncing the binaries with the Godot project'
+    throw
+  }
+  <#
+  .SYNOPSIS
+    Creates a copy of the built binaries on the corresponding folder of the Godot project.
+  #>
+}
+
+<#region MAIN #>
+# We store the original location and error action preference to restore them later
+$Script:originalLocation = Get-Location
+$Script:originalErrorAction = $ErrorActionPreference
+
+try {
+  $ErrorActionPreference = 'Stop' # This tells pwsh to throw an exception for **all** errors
+  Invoke-CleanCMake
+  Invoke-BuildCMake
+  Sync-Binaries
 } catch {
   Write-Output $_
   Write-Output $_.Exception
@@ -22,3 +60,4 @@ try {
   Set-Location $originalLocation
   $ErrorActionPreference = $originalErrorAction
 }
+<#endregion#>
