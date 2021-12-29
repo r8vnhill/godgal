4f1b9f4fb3d32729fe4e451f54295556a776943a
Beltrán Amenábar <beltra.am@gmail.com>
2021-12-29 12:00:59 -0300

    feat: add the convex hull algorithm to the godot example

--- project/Scripts/Main.gd
+++ project/Scripts/Main.gd
@@ -6,11 +6,12 @@ var stars
 var Star
 var Poligono = []
 
+onready var polygon = $Polygon2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    stars = []
-    Star = preload("../Escenas/Star.tscn")
+	stars = []
+	Star = preload("../Escenas/Star.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,36 +21,36 @@ func _ready():
 # warning-ignore:unused_argument
 # Interact with input
 func _input(event):
-    if Input.is_action_pressed("ui_click"):
-        var new_star = createStar()
-        stars.append(new_star)
-        get_tree().get_current_scene().add_child(new_star)
-        update()
-    if Input.is_action_pressed("ui_der_click"):
-        var new_star = createStar()
-        for star in stars:
-            if star == new_star:
-                stars.remove(star)
-                get_tree().get_current_scene().remove_child(star)
-        update()
+	if Input.is_action_pressed("ui_click"):
+		var new_star = createStar()
+		stars.append(new_star)
+		get_tree().get_current_scene().add_child(new_star)
+		update()
+	if Input.is_action_pressed("ui_der_click"):
+		var new_star = createStar()
+		for star in stars:
+			if star == new_star:
+				stars.remove(star)
+				get_tree().get_current_scene().remove_child(star)
+		update()
 
 # Creates a new instance of Star in the mouse position
 func createStar():
-    var new_star = Star.instance()
-    new_star.set_place(get_global_mouse_position())
-    return new_star		
+	var new_star = Star.instance()
+	new_star.set_place(get_global_mouse_position())
+	return new_star		
 
 # return the positions of the stars
 func get_positions():
-    var positions = []
-    for star in stars:
-        positions.append(star.get_place())
-    return positions
+	var positions = []
+	for star in stars:
+		positions.append(star.get_place())
+	return positions
 
 # Se llama cada vez que se hace update()
 # No se porque no dibuja al poligono :C
 func _draw():
-    var colors = PoolColorArray([Color(1,1,1)])
-    if stars.size() >=3:
-        Poligono = get_positions()
-        draw_polygon(Poligono,colors)
+	var colors = PoolColorArray([Color(1,1,1)])
+	if stars.size() >=3:
+		Poligono = get_positions()
+		polygon.draw_polygon(Poligono,colors)
